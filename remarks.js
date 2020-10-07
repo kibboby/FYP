8a45de448ad3a3b9b1a73d14d4ff6bb08b6f2002
WC991122 <1000864343@students.swinburne.edu.my>
2020-10-07 19:40:40 +0800

    remarks.js

---
+++ remarks.js
@@ -0,0 +1,99 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View,TextInput,TouchableOpacity } from 'react-native';
+
+
+//export default function App() {
+export default class Touchables extends Component {
+
+_onPressSubmitButton() {
+    alert('Nothing to Submit')
+  }
+
+state={
+remarksDescription:''
+};
+   
+render(){
+  return (
+    <View style={styles.container}>
+        <View  style={styles.RemarksC}>
+            <Text style={styles.Remarks}>Remarks</Text>
+            <TextInput 
+            style={styles.inputR}
+            placeholder='Write fown your justification here'
+            multiline={true}
+            autoFocus={true}
+            onChangeText={(text) => {this.setState(() => {
+                    return {
+                          remarksDescription: text
+                    };
+            })}}
+
+            />
+         </View>
+         <View>
+            <TouchableOpacity
+            style={styles.SubmitButtonR}
+            //onPress={this._onPressLoginButton}
+            //disabled={!this.state.isFormValid}
+            onPress={this._onPressSubmitButton}
+            >
+            <Text style={styles.SubmitR}>SUBMIT</Text>
+            </TouchableOpacity>
+          </View>
+
+      
+        
+        
+        
+        <StatusBar style="auto" />
+    </View>
+  );
+}}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'Left',
+    padding:"10%"
+    //justifyContent: 'center',
+  },
+
+  RemarksC:{
+  //margintop:50,
+  backgroundColor:"White",
+  },
+
+  Remarks:{
+  margintop:"10%",
+  color:"black",
+  fontWeight:"bold",
+  //fontSize:14,
+  },
+
+  inputR:{
+  marginTop:10,
+  padding:10,
+  //borderWidth:2,
+  //borderColor:'gray'
+  backgroundColor:'lightgrey',
+  height:200,
+  },
+
+  SubmitButtonR:{
+  marginTop: 20,
+  backgroundColor: "black",
+  padding: 10,
+  },
+
+  SubmitR:{
+  color:"white",
+  fontSize:20,
+  fontWeight:'bold',
+  justifyContent: "center",
+  textAlign: "center",
+  },
+
+});
