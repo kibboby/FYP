032b7a587d11ae4e53e501bc6914692d4721693b
WC991122 <1000864343@students.swinburne.edu.my>
2020-10-18 23:50:03 +0800

    added pages 2 wc where some files i cant access check whatsapp

---
+++ Page 2 WC/ListCompany_SuperAdmin.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, FlatList, TouchableOpacity, ScrollView } from 'react-native';
+import { Card } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default class ListofCompany extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            CompanyList: [
+                { CompanyName: 'Facebook' }
+                , { CompanyName: 'Facebook' }
+            ]
+        }
+    }
+
+    //to be further implemented by onPress function to go to company's detail page
+    getCompanyData = (item) => {
+        //var RollNo = item.RollNo;
+        //var StudentName = item.StudentName;
+        //var Course = item.Course;
+
+        //alert(RollNo + "\n" + StudentName + "\n" + Course);
+    }
+
+    //to implement changing between pages
+    _onPressButton() {
+        //alert('You tapped the button!')
+    }
+
+    render() {
+        return (
+            <View style={{ flex: 1, padding:"10%"}}>
+
+                <View style={{ flexDirection: 'row', justifyContent: 'flex-start', width:"65%" }}>
+                    <TouchableOpacity
+                        onPress={this._onPressButton}
+                        style={styles.nav}>
+                        <Text style={styles.navTitle}>Overall Report</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={this._onPressButton}
+                        style={styles.cardActive}>
+                        <Text style={styles.activeTitle}>Company Report</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={this._onPressButton}
+                        style={styles.nav}>
+                        <Text style={styles.navTitle}>Salesperson Report</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={this._onPressButton}
+                        style={styles.nav}>
+                        <Text style={styles.navTitle}>Leads Report</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <ScrollView>
+                    <FlatList
+                        data={this.state.CompanyList}
+                        renderItem={({ item }) =>
+
+                            <Card style={styles.card} onPress={() => this.getCompanyData(item)}>
+                                <View style={{ flexDirection: 'row' }}>
+                                    <Text style={styles.CompanyName}>{item.CompanyName}</Text>
+                                    <View style={{ justifyContent: "flex-end" }}>
+                                        <Icon name="right" size={15}/>
+                                    </View>
+                                </View>
+                            </Card>
+                        }
+                    />
+                </ScrollView>
+            </View>
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    nav: {
+        margin: 5,
+        backgroundColor: 'lightgrey',
+        padding: 5,
+        textAlign: 'center',
+        borderRadius: 5,
+    },
+    navTitle: {
+        fontSize: 12
+    },
+    cardActive: {
+        backgroundColor: 'black',
+        width: '25%',
+        textAlign: 'center',
+        borderRadius: 5
+    },
+    activeTitle: {
+        color: 'white',
+        fontSize: 12
+    },
+    pieChartArea: {
+        height: '30%',
+        borderColor: 1,
+        borderWidth: 1,
+        width: '60%',
+        alignSelf: 'center'
+    },
+    CompanyName: {
+        fontSize: 12,
+        flex: 1
+    },
+    card: {
+        backgroundColor: 'lightgrey',
+        margin: 5,
+    }
+});
---
+++ Page 2 WC/account_SalePrsn.js
@@ -0,0 +1,141 @@
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { Card } from 'react-native-paper';
+
+export default class SalesPersonAccount extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            TaskList: [
+                { Type: 'Others', Date: '25/11/2020' }
+            ]
+        }
+    }
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    padding:"10%"
+                }}>
+
+
+                <View style={styles.Direction}>
+                    <Icon
+                        name='user'
+                        size={55}
+                        style={styles.profileImg} />
+                    <View>
+                        <Text style={styles.Username}>
+                            John David
+                    </Text>
+                        <Text style={styles.designation}>
+                            Salesperson
+                    </Text>
+                    </View>
+                </View>
+                <View style={styles.Direction}>
+                    <View style={styles.Text}>
+                        <Text style={styles.TextMargin}>Name</Text>
+                        <Text style={styles.TextMargin}>Email</Text>
+                        <Text style={styles.TextMargin}>Contact</Text>
+                    </View>
+
+                    <View style={styles.Info}>
+                        <Text style={styles.TextMargin}>John David Beckham</Text>
+                        <Text style={styles.TextMargin}>abc@gmail.com</Text>
+                        <Text style={styles.TextMargin}>+6 012 345 6789</Text>
+                    </View>
+                </View>
+
+                <Text style={styles.TaskTitle}>
+                    UPCOMING TASKS
+            </Text>
+
+
+            <FlatList
+                    data={this.state.TaskList}
+                    renderItem={({ item }) =>
+
+                        <Card style={styles.card}>
+                            <View style={styles.Task}>
+                                    <Text style={styles.Type}>{item.Type}</Text>
+                                    <Text style={styles.Date}> | </Text>
+                                    <Text style={styles.Date}>{item.Date}</Text>
+                            </View>
+                        </Card>
+                    }
+                    keyExtractor={item => item.ID}
+                />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    Direction: {
+        flexDirection: 'row',
+        marginTop: 10
+    },
+    profileImg: {
+        borderRadius: 50,
+        marginStart: 10,
+        marginTop: 2.5,
+        height: 70,
+        width: 70,
+        overflow: 'hidden',
+        borderColor: 'black',
+        borderWidth: 1,
+        paddingStart: 15,
+        paddingTop: 4
+    },
+    Username: {
+        marginLeft: 15,
+        marginTop: 10,
+        fontSize: 20,
+    },
+    designation: {
+        marginLeft: 15,
+        fontSize: 12,
+    },
+    Info: {
+        marginTop: 2.5,
+        marginStart: 35,
+        fontSize: 14,
+    },
+    Text: {
+        marginTop: 2.5,
+        marginLeft: 15,
+        fontSize: 14,
+    },
+    TextMargin: {
+        marginBottom: 5
+    },
+    TaskTitle: {
+        fontFamily: 'Roboto',
+        fontSize: 16,
+        marginTop: 20,
+        marginStart: 15,
+        fontWeight: 'bold'
+    },
+    Task: {
+        flexDirection: 'row',
+        marginTop: 5,
+        backgroundColor: 'lightgrey',
+        padding: 7,
+        marginBottom: 2,
+        flexDirection: 'row',
+    },
+    Date: {
+        marginStart: 5,
+    },
+    card: {
+        margin: 5,
+        backgroundColor: 'lightgrey',
+        borderRadius: 10
+    },
+});
